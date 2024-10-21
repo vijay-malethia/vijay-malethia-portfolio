6a637f87552fa9c9b4836f1d4d9077909ca48931
Vijay Malethia <vjmalethiacwd@gmail.com>
2024-10-21 22:56:43 +0530

    create portfolio web app

---
+++ lib/view/intro/components/social_media_coloumn.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/view/intro/components/social_icon.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SocialMediaIconColumn extends StatelessWidget {
+  const SocialMediaIconColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SocialMediaIcon(
+            icon: 'assets/icons/linkedin.svg',
+            onTap: () => launchUrl(
+                Uri.parse('https://www.linkedin.com/in/vijay-malethia/'))),
+        SocialMediaIcon(
+          icon: 'assets/icons/github.svg',
+          onTap: () =>
+              launchUrl(Uri.parse('https://github.com/Vijay-Malethia')),
+        ),
+        const SocialMediaIcon(
+          icon: 'assets/icons/dribble.svg',
+        ),
+        const SocialMediaIcon(icon: 'assets/icons/twitter.svg'),
+        const SocialMediaIcon(icon: 'assets/icons/linkedin.svg'),
+      ],
+    );
+  }
+}
